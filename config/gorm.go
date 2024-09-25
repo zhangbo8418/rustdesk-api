036f89ee13ea33061b93532562198bd56c44e440
裙下孤魂 <46797568+zhangbo8418@users.noreply.github.com>
2024-09-25 09:09:18 +0800

    Update gorm.go

--- config/gorm.go
+++ config/gorm.go
@@ -9,10 +9,12 @@ type Gorm struct {
 	Type         string `mapstructure:"type"`
 	MaxIdleConns int    `mapstructure:"max-idle-conns"`
 	MaxOpenConns int    `mapstructure:"max-open-conns"`
+	Dbpath       string `mapstructure:"dbpath"`
 }
 
 type Mysql struct {
 	Addr     string `mapstructure:"addr"`
+	Socket   string `mapstructure:"socket"`
 	Username string `mapstructure:"username"`
 	Password string `mapstructure:"password"`
 	Dbname   string `mapstructure:"dbname"`
