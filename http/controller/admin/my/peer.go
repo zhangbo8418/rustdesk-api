1b9a2d93bacda006ed2b2aaf9bae1364d87227e1
裙下孤魂 <46797568+zhangbo8418@users.noreply.github.com>
2025-02-21 15:58:20 +0800

    Update peer.go

--- http/controller/admin/my/peer.go
+++ http/controller/admin/my/peer.go
@@ -3,7 +3,7 @@ package my
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/lejianwen/rustdesk-api/v2/http/request/admin"
-	"github.com/zhangbo8418/rustdesk-api/v2/http/controller/api"
+	"github.com/lejianwen/rustdesk-api/v2/http/controller/api"
 	"github.com/lejianwen/rustdesk-api/v2/http/response"
 	"github.com/lejianwen/rustdesk-api/v2/service"
 	"gorm.io/gorm"
